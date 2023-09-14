3ad700c605ab55608a60dfa288f1d64a3560e53b
unknown <dimassetiawan5.7y@gmail.com>
2023-09-14 20:53:17 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// object literation
+
+let murid = {
+    nama : 'dimas ',
+    kelas : 'XII TKJ 1',
+    nisn : 4439915,
+    data : function() {
+        console.log(this.nama + 'ada di kelas ' + this.kelas + ' dengan nisn ' + this.nisn);
+    }
+}
